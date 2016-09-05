470a7410aa9cd01ce62c4369bc9371404670cff5
Fernando Boza <hi@fernandoboza.com>
2016-09-05 03:10:32 +0200

    v1 > user login

--- server.js
+++ server.js
@@ -4,10 +4,27 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var users = [];
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  //new user
+  socket.on('join', function(data){
+      console.log(data);
+      console.log(users);
+      //User name
+      socket.nickname = data.nickname;
+      users[socket.nickname] = socket;
+      var userObj = {
+          nickname: data.nickname,
+          socketID: socket.id
+      };
+      users.push(userObj);
+      io.emit('all-users', users);
+  });
 
  socket.on('disconnect', function(){
     console.log('user disconnected');
